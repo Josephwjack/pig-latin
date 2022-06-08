6f96469116d813308d1ec4b704d46693d80224ce
Josephwjack <joseph.w.jack@gmail.com>
2022-06-08 15:50:21 -0700

    add rule for words starting with consonants to be placed at end of with with 'ay'
    Co-Authored-by: Anthony DiFalco

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,19 @@ function pigLatin (text) {
   for (let index = 0; index < wordArray.length; index++) {
   let newArray = wordArray[index].split("");
     if (vowels.includes(newArray[0].toLowerCase())) {
-      pigArray.push(wordArray[index] + "way ");
+      pigArray.push(wordArray[index] + "way");
     } else { 
       for (let index = 0; index < newArray.length; index++) {         // for each letter
         if (vowels.includes(newArray[index].toLowerCase())) {         // if letter is a vowel
           let array2 = newArray.slice(index, (newArray.length));      // slice the array at the current index
           let array1 = newArray.slice(0, index);                      //(array1 = first half) (array2 = second helf)
-          pigArray.push(array2.join("") + array1.join("") + "ay ");   //array2 + array1 + "ay"
+          pigArray.push(array2.join("") + array1.join("") + "ay");   //array2 + array1 + "ay"
           break;                                                      // break
         }
       }
+    }
+    if (index !== wordArray.length-1) {
+      pigArray.push(" ");
     }
   } 
   return pigArray.join("");
